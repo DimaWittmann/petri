991282c5e594bd6b6be5f44bedc6bb5d5a7f5d5b
DimaWittmann <dimawittmann@gmail.com>
2012-10-11 23:06:17 +0300

    Зроблена примітивне відображення швидкодії

--- Modeling.java
+++ Modeling.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -10,7 +11,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.io.Console;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 
 /**
@@ -18,56 +21,119 @@ import java.io.Console;
  * @author Wittmann
  */
 public class Modeling {
-
-
+    static double curTime=0;
+    static double dTime=0.1;
+    static View view;
+    
+    public Modeling(){
+        JLabel lCPU=new JLabel("CPU ", SwingConstants.LEFT);
+        
+    }
 	
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
        
         Device CPU =new Device("CPU",1, false);
         Device RAM=new Device("RAM",1, true);
+        Device outSBridge = new Device("outSBridge",2,true);
+        Device inSBridge = new Device("inSBridge",2,true);
+        Device outNBridge = new Device("outDBridge",4,true);
+        Device inNBridge = new Device("inDBridge",4,true);
+        Device DC = new Device("DC", 43, true); 
+        Device USB = new Device("USB",20,true);
+        Device ISA = new Device("ISA",25,true);
+        Device LPT = new Device("LPT",50,true);
+        Device AU = new Device("AU",53,true);
+        Device VP = new Device("VP",32,true);
         
-        CPU.addConnection(CPU,0.5);
-        CPU.addConnection(RAM,0.5);
-        RAM.addConnection(CPU,1);
+        view =new View();
+        
+        Device [] devices = {CPU, RAM,outSBridge,inSBridge, outNBridge,
+            inNBridge,DC,USB,ISA,LPT,AU,VP};
+        
+        CPU.addConnection(CPU,0.8);
+        CPU.addConnection(inSBridge,0.2);
+        
+        inSBridge.addConnection(inNBridge, 0.25);
+        inSBridge.addConnection(RAM, 0.75);
+        
+        RAM.addConnection(outSBridge,1);
+        outSBridge.addConnection(CPU, 1);
+        
+        inNBridge.addConnection(USB, 0.25);
+        inNBridge.addConnection(DC, 0.35);
+        inNBridge.addConnection(ISA, 0.1);
+        inNBridge.addConnection(AU, 0.1);
+        inNBridge.addConnection(VP, 0.1);
+        
+        outNBridge.addConnection(outSBridge, 1);
+        
+        USB.addConnection(outNBridge, 0.6);
+        USB.addConnection(ISA, 0.4);
+        
+        DC.addConnection(outNBridge, 1);
+        
+        ISA.addConnection(LPT, 0.5);
+        ISA.addConnection(outNBridge, 0.5);
+        
+        LPT.addConnection(CPU, 1);
+        
+        AU.addConnection(CPU, 1);
+        
+        VP.addConnection(CPU, 1);
         
         ArrayList<Task> t=new ArrayList<>();
         
         t.add(new Task("T1"));
         t.add(new Task("T2"));
+        t.add(new Task("T2"));
         
         for (int i=0;i<t.size();i++){
             CPU.addTask(t.get(i));
         }
-
-        double curTime=0;
-        double dTime=0.1;
-
         
-        
-		
-        while (true){
-            
-            for (int i=0;i<t.size()-1;i++){
-                if(curTime>=t.get(i).time){
-                    t.get(i).goFuther();
+        view.setVisible(true);
+
+
+        while(true){
+            int j=30000000;
+
+            while (j>0){
+                j--;
+                for (int i=0;i<t.size();i++){
+                if(curTime>=t.get(0).time){
+                    t.get(0).goFuther();
                 }
-            }
-            
-            curTime+=dTime;
-            
+                
 
+                curTime+=dTime;
+                for (i=0;i<devices.length;i++) {
+                    view.labels[i].setText(showPerf(devices[i]));
+                } 
+                Thread.sleep(1);
 
-            double perf =  Math.round((CPU.p.timeOfExec/curTime<1)? CPU.p.timeOfExec/curTime * 100 : 100.0);
-            System.out.print("CPU:"+ perf + " %         ");
-            perf =  Math.round((RAM.p.timeOfExec/curTime<1)? RAM.p.timeOfExec/curTime * 100 : 100.0);
-            System.out.println("RAM:"+perf+"%");
-            //getKey();
+            }           
+                
+            }        
+                
+
+            System.out.println(curTime);
+
         }
+        
+        
+        
     }
     
+    
+    
+  
+    public static String showPerf(Device device){
+            double perf =  ((device.p.timeOfExec/curTime<1)? device.p.timeOfExec/curTime * 100 : 100.0);
+            return ((int)(perf)+"%");
+    }
  
     
     public static void getKey(){
